009478e73f8bb054099bb8f636609645e7f132d1
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2020-10-09 11:47:32 +0200

    remove grey styling

--- src/components/page-sections/our-partners/OurPartners.jsx
+++ src/components/page-sections/our-partners/OurPartners.jsx
@@ -35,10 +35,6 @@ const OurPartners = () => (
             <Image
               src={value}
               width="100%"
-              style={{
-                filter: "grayscale(1)",
-                opacity: index === 1 || index === 3 ? "0.3" : 1,
-              }}
             />
           </Pane>
         ))}
@@ -48,3 +44,13 @@ const OurPartners = () => (
 )
 
 export default OurPartners
+
+/* old grey-out styling for the <Image> elements
+    removed so the logos will be colored
+    preserved for posterity
+
+style={{
+  filter: "grayscale(1)",
+  opacity: index === 1 || index === 3 ? "0.3" : 1,
+}}
+*/
