2edafd3b900b7cf50a2249a53e9545341fd6b485
Lien Arits <62723549+iLienstAr@users.noreply.github.com>
2020-12-04 09:49:37 +0100

    edit application form

--- src/components/navigation/top-nav/TopNav.jsx
+++ src/components/navigation/top-nav/TopNav.jsx
@@ -26,7 +26,7 @@ const TopNav = ({ onBurgerNavClicked, isMobile, currentPage }) => {
         </NavLink>
         <Button
           is="a"
-          href="https://docs.google.com/forms/d/e/1FAIpQLSd6V6JyhWH3M4dhmXnAWcdMHCVEdcdQJyT1DzXZodvHufSrJQ/viewform?usp=sf_link"
+          href="https://docs.google.com/forms/d/e/1FAIpQLSfS0215I6BCy1jzpWmxv9UxYxp5I25ZcoWhSxijhcUImLELcg/viewform"
           target="_blank"
           rel="noopener noreferrer nofollow"
         >
