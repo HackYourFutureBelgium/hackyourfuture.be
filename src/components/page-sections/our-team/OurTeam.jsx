a7576b841763948dbe5d17250454de302304e644
Jonathan Beliën <dev@jbelien.be>
2021-01-12 17:11:00 +0100

    Update OurTeam.jsx
    Remove Lien empty Twitter link.

--- src/components/page-sections/our-team/OurTeam.jsx
+++ src/components/page-sections/our-team/OurTeam.jsx
@@ -14,7 +14,6 @@ const data = {
       role: "General Coordinator",
       links: [
         { icon: "mail", link: "mailto:lien@hackyourfuture.be" },
-        { icon: "twitter", link: "" },
         {
           icon: "linkedin",
           link: "https://www.linkedin.com/in/lien-arits-88722683/",
