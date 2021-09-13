5b59e7dae24220287b69549581ee1917acfc944a
G <62214717+gelilaa@users.noreply.github.com>
2021-09-13 15:35:41 +0200

    Update TopNav.jsx
    currently closed (13/09/2021)

--- src/components/navigation/top-nav/TopNav.jsx
+++ src/components/navigation/top-nav/TopNav.jsx
@@ -26,7 +26,7 @@ const TopNav = ({ onBurgerNavClicked, isMobile, currentPage }) => {
         </NavLink>
         <Button
           is="a"
-          href="https://forms.gle/fdX4C88nDK6iK8BT9"
+          href="https://forms.gle/QXh3gMNVhWbMFhsw6"
           target="_blank"
           rel="noopener noreferrer nofollow"
         >
