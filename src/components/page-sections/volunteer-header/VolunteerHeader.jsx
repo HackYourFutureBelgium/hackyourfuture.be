807267c855a009eaa91b9008e44ea91476f6a915
gelilaa <gelila.ali@outlook.com>
2022-01-20 15:23:56 +0100

    feature:added new form coaches

--- src/components/page-sections/volunteer-header/VolunteerHeader.jsx
+++ src/components/page-sections/volunteer-header/VolunteerHeader.jsx
@@ -8,7 +8,7 @@ const data = {
     "HackYourFuture is possible only thanks to our many volunteer coaches and mentors. Do you also want to help to make the tech industry more inclusive and become part of our community?",
   cta1: "join as a coach",
   cta1Link:
-    "https://docs.google.com/forms/d/e/1FAIpQLSef3OH8546MJNllcvrv7KomHelqzrKNpnLRVbI1ZqbeajnluA/viewform",
+    "https://form.typeform.com/to/yfzzQxtI",
   cta2: "Join as a mentor",
   cta2Link:
     "https://docs.google.com/forms/d/e/1FAIpQLSdsTmIgbCA2X02NUFsVSE-BQJIx6_KWpABujlZYA9PP8JgHJg/viewform",
