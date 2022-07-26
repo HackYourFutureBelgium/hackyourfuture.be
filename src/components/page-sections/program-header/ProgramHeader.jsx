f0c99c602fec7b222793f4296173b2d2e0a34c81
Daniel Halasz <halaszdaniel@gmail.com>
2022-07-26 11:10:38 +0200

    changed program header

--- src/components/page-sections/program-header/ProgramHeader.jsx
+++ src/components/page-sections/program-header/ProgramHeader.jsx
@@ -10,7 +10,7 @@ const data = {
   title: "Your new career starts here",
   description:
     "HackYourFuture is a <b>free 9-month front end web development program</b> for newcomers passionate about code and web development. You don’t need any technical background to start, only loads of motivation.<br/><br/>If you want to kickstart your career in the IT sector, take your chance!<br/><b>Women are strongly encouraged to apply!</b><br><br>",
-  cta1: "Applications for new classes is constantly ongoing",
+  cta1: "Applications for new classes are constantly ongoing",
   cta: "Apply Now",
   ctaLink: "https://form.typeform.com/to/SQbn9nP1",
   image: imageUrl,
