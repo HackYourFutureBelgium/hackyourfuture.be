75857afd3d82e48a39fee2dc20fac46ac0f9f8f4
Kevin Tassi <kevin.tassi@gmail.com>
2022-12-02 13:47:49 +0100

    Update stripe keys

--- src/utils/stripe/index.js
+++ src/utils/stripe/index.js
@@ -3,8 +3,8 @@ import { loadStripe } from "@stripe/stripe-js"
 let stripePromise
 const getStripe = () => {
   if (!stripePromise) {
-    stripePromise = loadStripe("pk_live_mFpKP0JmQWp9mQ2FjetxyzlH", {
-      stripeAccount: "acct_1DUXo0B6dm2WDTHv",
+    stripePromise = loadStripe("pk_live_51LtuIuAwSpPsVM3a17NgcmkhQ7WWTlX4dXWoMcEi6i5oxWIQnQeJouneqq6TcsagoGEKKSlHEig8mGkhKLNpWuTm00ik9P6nxi", {
+      stripeAccount: "acct_1LtuIuAwSpPsVM3a",
     })
   }
   return stripePromise
