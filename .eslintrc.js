514852cef12881795cfacbc8a1c2a0a427fe4b43
Sebastian Herrera <96073676+sebherrerabe@users.noreply.github.com>
2023-03-07 16:40:12 +0100

    Setting Up Storybook (#270)
    * init storybook with npx
    
    * delete unnecessary components
    
    * Rename/Reorder scripts
    
    * Remove coma
    
    * Rename script
    
    * Update README
    
    ---------
    
    Co-authored-by: Kevin <kevin.tassi@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,19 @@
 module.exports = {
   globals: {
-    __PATH_PREFIX__: true,
+    __PATH_PREFIX__: true
   },
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  plugins: [
+    "@typescript-eslint"
+  ],
   extends: [
     "react-app",
     "eslint:recommended",
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:storybook/recommended"
   ],
   rules: {
-    "no-inner-declarations": 1,
-  },
-}
+    "no-inner-declarations": 1
+  }
+};
