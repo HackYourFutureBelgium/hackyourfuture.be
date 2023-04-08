e6bb62de2a3047f5599008db45da15b228407a5f
Sebastian Herrera <sebastianherrerac1997@gmail.com>
2023-04-08 15:08:35 +0200

    Improve styles of error container

--- src/components/ui/input/InputV2.tsx
+++ src/components/ui/input/InputV2.tsx
@@ -8,45 +8,67 @@ export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   isRequired?: boolean
 }
 
-const Input = ({ name, label, error, isRequired, ...props }: InputProps) => {
+const Input = ({
+  name,
+  label,
+  error,
+  isRequired,
+  ...props
+}: InputProps) => {
   return (
     <StyledInputDiv>
       {label && (
         <label htmlFor={name}>
-          {label}{isRequired && "*"}
+          {label}
+          {isRequired && "*"}
         </label>
       )}
-      <StyledInput name={name} {...props} />
-      {error && <StyledInputErrorDiv>{error}</StyledInputErrorDiv>}
+      <StyledInput name={name} error={error} {...props} />
+      <StyledInputErrorDiv>
+        <span>{error}</span>
+      </StyledInputErrorDiv>
     </StyledInputDiv>
   )
 }
 
 export default Input
-
 const StyledInputDiv = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 8px;
+  gap: 0.5rem;
   width: 100%;
   font-family: "Poppins", sans-serif;
   font-weight: 500;
+  margin-bottom: 1.5rem;
+  position: relative;
 `
-const StyledInput = styled.input`
-  border: 1px solid rgba(24, 79, 244, 0.3);
-  box-shadow: 0px 0px 0px 4px rgba(24, 79, 244, 0.05);
+
+type StyledInputProps = Pick<InputProps, "error">
+
+const StyledInput = styled.input<StyledInputProps>`
+  border: 1px solid
+    ${({ error }) =>
+      error ? "rgba(250, 0, 0, 0.3)" : "rgba(24, 79, 244, 0.3)"};
+  box-shadow: 0 0 0 0.25rem
+    ${({ error }) =>
+      error ? "rgba(250, 0, 0, 0.05)" : "rgba(24, 79, 244, 0.05)"};
   background: #ffffff;
-  border-radius: 8px;
-  padding: 10px 16px;
+  border-radius: 0.5rem;
+  padding: 0.625rem 1rem;
   font-size: 16px;
   transition: border 0.1s ease-in-out, box-shadow 0.1s ease-in-out;
   &:focus {
-    outline: 2px solid #184ff4;
-    box-shadow: 0px 0px 0px 4px rgba(24, 79, 244, 0.2);
+    outline: 2px solid ${({ error }) => (error ? "#fa0000" : "#184ff4")};
+    box-shadow: 0 0 0 0.25rem
+      ${({ error }) =>
+        error ? "rgba(250, 0, 0, 0.2)" : "rgba(24, 79, 244, 0.2)"};
   }
 `
 const StyledInputErrorDiv = styled.div`
   color: #ff0000;
   font-size: 12px;
-  font-weight: 400;
+  font-weight: 500;
+  position: absolute;
+  top: calc(100% + 0.25rem);
+  z-index: 1;
 `
