0a4aa248647e17dac0b550e892ebc9748fabb254
Sebastian Herrera <sebastianherrerac1997@gmail.com>
2023-04-11 18:33:47 +0200

    Add tests to each story

--- src/stories/Input.stories.tsx
+++ src/stories/Input.stories.tsx
@@ -1,5 +1,7 @@
-import React from "react"
+import { expect } from "@storybook/jest"
 import { Meta, Story } from "@storybook/react"
+import { userEvent, within } from "@storybook/testing-library"
+import React from "react"
 import Input, { InputProps } from "../components/ui/input/InputV2"
 
 const meta: Meta<InputProps> = {
@@ -88,12 +90,45 @@ export default meta
 
 const Template: Story<InputProps> = args => <Input {...args} />
 
+interface TestArgs {
+  canvasElement: HTMLElement
+  story: Story<InputProps>
+  value: string
+}
+
+const testInput = async ({ canvasElement, story, value }: TestArgs) => {
+  const canvas = within(canvasElement)
+
+  const input = await canvas.getByPlaceholderText(
+    story.args?.placeholder as string
+  )
+
+  expect(input).toBeInTheDocument()
+
+  await userEvent.type(input, value)
+  expect(input).toHaveValue(value)
+
+  await userEvent.clear(input)
+  expect(input).toHaveValue("")
+
+  await userEvent.click(input)
+  expect(input).toHaveFocus()
+
+  input.blur()
+
+  return input
+}
+
 export const Default = Template.bind({})
 
 Default.args = {
   placeholder: "Enter your name...",
   type: "text",
   id: "name",
+}
+
+Default.play = async ({ canvasElement }) => {
+  testInput({ canvasElement, story: Default, value: "John Doe" })
 }
 
 export const WithLabel = Template.bind({})
@@ -103,6 +138,28 @@ WithLabel.args = {
   type: "text",
   id: "name",
   label: "Name",
+}
+
+WithLabel.play = async ({ canvasElement }) => {
+  const input = await testInput({
+    canvasElement,
+    story: WithLabel,
+    value: "John Doe",
+  })
+
+  const canvas = within(canvasElement)
+
+  const label = await canvas.getByText(WithLabel.args?.label as string, {
+    selector: "label",
+  })
+
+  expect(label).toBeInTheDocument()
+  expect(label).toHaveAttribute("for", WithLabel.args?.id as string)
+
+  await label.click()
+  expect(input).toHaveFocus()
+
+  input.blur()
 }
 
 export const WithError = Template.bind({})
@@ -115,6 +172,22 @@ WithError.args = {
   isRequired: true,
 }
 
+WithError.play = async ({ canvasElement }) => {
+  await testInput({
+    canvasElement,
+    story: WithError,
+    value: "John Doe",
+  })
+
+  const canvas = within(canvasElement)
+
+  const error = await canvas.getByText(WithError.args?.error as string, {
+    selector: "span",
+  })
+
+  expect(error).toBeInTheDocument()
+}
+
 export const AsTextArea = Template.bind({})
 
 AsTextArea.args = {
@@ -124,4 +197,16 @@ AsTextArea.args = {
   as: "textarea",
   rows: 6,
   resize: "none",
+}
+
+AsTextArea.play = async ({ canvasElement }) => {
+  const input = await testInput({
+    canvasElement,
+    story: AsTextArea,
+    value: "John Doe",
+  })
+
+  expect(input).toHaveAttribute("rows", AsTextArea.args?.rows?.toString())
+  expect(input).toHaveStyle(`resize: ${AsTextArea.args?.resize}`)
+  expect(input.tagName).toBe("TEXTAREA")
 }
